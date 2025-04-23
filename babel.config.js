5ef201c4fefd98d5566d3a3a985e445bd68049f0
GloFlav <rasgloflav@gmail.com>
2025-04-23 15:07:29 +0300

    not fix

--- babel.config.js
+++
@@ -1,5 +0,0 @@
-/** @format */
-
-module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-react"],
-};
--- jest.config.js
+++
@@ -1,5 +0,0 @@
-/** @format */
-
-module.exports = {
-  testEnvironment: "jest-environment-jsdom",
-};
